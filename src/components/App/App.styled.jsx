a3d0807c152a5b239ded4d6e846a0ad1f5cab582
Ihor Statsenko <statsenkoin@gmail.com>
2023-02-19 23:51:19 +0200

    Create App.styled.jsx

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #010101;
+  font-size: 28px;
+`;
