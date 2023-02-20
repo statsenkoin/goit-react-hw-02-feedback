12f75a9e6ecfe1ccd52179fdefcc5e4fc392f5e3
Ihor Statsenko <statsenkoin@gmail.com>
2023-02-20 22:35:34 +0200

    Feature/ add Section component

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,3 +1,12 @@
 import React from 'react';
 
-const Section = () => {};
+const Section = ({ title, children }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      {children}
+    </div>
+  );
+};
+
+export { Section };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
-    <>
+    <div>
       <span>Good: {good}</span>
       <span>Neutral: {neutral}</span>
       <span>Bad: {bad}</span>
       <span>Total: {total}</span>
       <span>Positive feedback: {positivePercentage}%</span>
-    </>
+    </div>
   );
 };
 
