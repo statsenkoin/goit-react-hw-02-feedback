02dc4cc1d203136cdff46fa30be9e416c5df15b3
Ihor Statsenko <statsenkoin@gmail.com>
2023-02-20 23:59:58 +0200

    Feature/ add styled components

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
-// import { Title, ButtonPlate } from './FeedbackOptions.styled';
+import PropTypes from 'prop-types';
+import { Option, OptionList } from './FeedbackOptions.styled';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
-    <ul>
+    <OptionList>
       {Object.keys(options).map(key => {
         return (
           <li key={key}>
-            <button type="button" id={key} onClick={onLeaveFeedback}>
+            <Option type="button" id={key} onClick={onLeaveFeedback}>
               {options[key]}
-            </button>
+            </Option>
           </li>
         );
       })}
-    </ul>
+    </OptionList>
   );
 };
 
 export { FeedbackOptions };
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.shape({
+    good: PropTypes.string.isRequired,
+    neutral: PropTypes.string.isRequired,
+    bad: PropTypes.string.isRequired,
+  }).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -1,9 +1,25 @@
 import styled from 'styled-components';
 
-export const Title = styled.h2`
-  font-size: 28px;
-`;
-export const ButtonPlate = styled.div`
+export const OptionList = styled.ul`
   display: flex;
-  gap: 10px;
+  justify-content: center;
+  gap: 20px;
+  list-style: none;
+  padding: 0;
+  margin: 20px 0 0 0;
+`;
+
+export const Option = styled.button`
+  width: 100px;
+  padding: 5px;
+  font-size: 18px;
+  font-weight: 600;
+  background-color: #b0fefe;
+  border: 1px solid #3acdcd;
+  border-radius: 8px;
+
+  &:hover {
+    background-color: #96f1f1;
+    scale: 1.1;
+  }
 `;
