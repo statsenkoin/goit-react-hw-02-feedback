1513922146da27ad985db30163f0b854e1695ddc
Ihor Statsenko <statsenkoin@gmail.com>
2023-02-20 22:37:00 +0200

    Update App.styled.jsx

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 
 export const Layout = styled.div`
   height: 100vh;
+  width: 100%;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
   display: flex;
   flex-direction: column;
   justify-content: center;
