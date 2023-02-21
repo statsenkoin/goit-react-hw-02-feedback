1fbcc9995e311462d3f66a3a85661d4379adccf2
Ihor Statsenko <statsenkoin@gmail.com>
2023-02-21 15:51:17 +0200

    Fix/ update styles

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -2,12 +2,9 @@ import styled from 'styled-components';
 
 export const SectionWrap = styled.div`
   width: 400px;
-  padding: 20px;
+  padding: 20px 20px 0 20px;
   text-align: center;
   background-color: #e6f5f5;
-
-  box-shadow: rgb(0 0 0 / 20%) 0px 3px 3px -2px,
-    rgb(0 0 0 / 14%) 0px 3px 4px 0px, rgb(0 0 0 / 12%) 0px 1px 8px 0px; ;
 `;
 
 export const Title = styled.h2`
