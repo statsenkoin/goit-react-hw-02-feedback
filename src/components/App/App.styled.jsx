311f35adbcce2d9ac3d391ac06c9e84126375470
Ihor Statsenko <statsenkoin@gmail.com>
2023-02-21 14:38:30 +0200

    Feature/ add box shadow to container

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
 
 export const Layout = styled.div`
-  height: 100vh;
-  width: 100%;
-  max-width: 1200px;
+  width: 400px;
+  margin-top: 100px;
   margin-left: auto;
   margin-right: auto;
   display: flex;
@@ -12,4 +11,7 @@ export const Layout = styled.div`
   align-items: center;
   color: #010101;
   font-size: 28px;
+
+  box-shadow: rgb(0 0 0 / 20%) 0px 3px 3px -2px,
+    rgb(0 0 0 / 14%) 0px 3px 4px 0px, rgb(0 0 0 / 12%) 0px 1px 8px 0px; ;
 `;
